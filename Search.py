25f687d55f95e1df5ca3ea78af180ecc8375f3a3
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2020-11-16 13:25:43 +0900

    search file modify

--- Search.py
+++ Search.py
@@ -9,28 +9,19 @@ class_num = int(classof[1])
 
 if class_num <= 6 and int(classof[2:]) <= 20:
     if class_num == 1:
-        row_num = csv_file.loc[[index], ['pc', 'phone']]
-        print(row_num)
+        index += 0
     elif class_num == 2:
         index += 20
-        row_num = csv_file.loc[[index], ['pc', 'phone']]
-        print(row_num)
     elif class_num == 3:
         index += 40
-        row_num = csv_file.loc[[index], ['pc', 'phone']]
-        print(row_num)
     elif class_num == 4:
         index += 60
-        row_num = csv_file.loc[[index], ['pc', 'phone']]
-        print(row_num)
     elif class_num == 5:
         index += 80
-        row_num = csv_file.loc[[index], ['pc', 'phone']]
-        print(row_num)
     elif class_num == 6:
         index += 100
-        row_num = csv_file.loc[[index], ['pc', 'phone']]
-        print(row_num)
 else :
     print('학번이 잘못되었습니다.')
 
+row_num = csv_file.loc[[index], ['pc', 'phone']]
+print(row_num)
--- write_csv.py
+++ write_csv.py
@@ -1,6 +1,8 @@
-import csv
 
-#fields = ['날짜','학번','이름','점수','이유(100점 미만일 경우)']
+#input date
+date = input('날짜를 입력하세요 : ')
+
+
 myCsRow = "20.11.12,2302,강채현,100,null\n"
 with open('mypc_dummy.csv','a',encoding='utf-8') as fd :
   fd.write(myCsRow)
