1024d9396a83272d8731bb8b2bd15b48627372cd
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-19 14:45:00 +0900

    locate image btn

--- gui/main.py
+++ gui/main.py
@@ -1,14 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("IPC")
 window.config(bg='#272727')
-window.geometry("700x450+100+100")
+window.geometry("1000x650+100+100")
 window.resizable(False, False)
 
+title = Label(window, text="IPC", bg='#272727', fg='#51F591', font=("Arial 60 bold"))
+explain = Label(window, text=": 앱설명앱설명앱설명앱설명앱설명앱설명앱설명앱설명", bg='#272727', fg='#51F591', font=("System 13 normal italic"))
 
-label = tkinter.Label(window, text="IPC", bg='#272727', fg='#51F591', font=("Arial 40 bold"))
-label.place(x=80, y=90, anchor='nw')
+
+student_btn = PhotoImage(file='../image/student_btn.png')
+student_label = Label(image=student_btn, bg='#272727')
+
+teacher_btn = PhotoImage(file='../image/teacher_btn.png')
+teacher_label = Label(image=teacher_btn, bg='#272727')
+
+title.place(x=140, y=180, anchor='nw')
+explain.place(x=140, y=270, anchor='nw')
+student_label.place(x=140, y=360)
+teacher_label.place(x=320, y=360)
 
 window.mainloop()
