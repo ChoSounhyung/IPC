42a9c70114dffbcb30adeddd11886990c3185eb2
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-24 08:59:27 +0900

    teacher page init

---
+++ gui/test.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+def teacher():
+    window = Tk()
+    window.title("IPC")
+    window.config(bg='#272727')
+    window.geometry("1000x650+250+70")
+    window.resizable(False, False)
+    window.mainloop()
+
+if __name__ == '__main__':
+    teacher()
