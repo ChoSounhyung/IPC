44286d48d27ed82118002ba3c55038cd94ed61d9
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-24 08:35:50 +0900

    event function

--- gui/main.py
+++ gui/main.py
@@ -4,11 +4,16 @@ def main():
     window = Tk()
     window.title("IPC")
     window.config(bg='#272727')
-    window.geometry("1000x650+100+100")
+    window.geometry("1000x650+250+70")
     window.resizable(False, False)
     StartPage(window)
     window.mainloop()
 
+def student_page(event):
+    print("Student")
+
+def teacher_page(event):
+    print("Teacher")
 
 class StartPage(Frame):
     def __init__(self, master):
@@ -19,10 +24,12 @@ class StartPage(Frame):
         student_btn = PhotoImage(file='../image/student_btn.png')
         student_label = Label(image=student_btn, bg='#272727')
         student_label.image = student_btn
+        student_label.bind("<Button-1>", student_page)
 
         teacher_btn = PhotoImage(file='../image/teacher_btn.png')
         teacher_label = Label(image=teacher_btn, bg='#272727')
         teacher_label.image = teacher_btn
+        teacher_label.bind("<Button-1>", teacher_page)
 
         title.place(x=140, y=180, anchor='nw')
         explain.place(x=140, y=270, anchor='nw')
