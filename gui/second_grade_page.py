1fda057642a856a8854444c27e0d7a2314da5f27
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-12-01 01:22:44 +0900

    real ip modify & grade init

---
+++ gui/second_grade_page.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class SecondGradePage:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("IPC")
+        self.window.geometry("1000x650+250+70")
+        self.window.config(bg='#272727')
+        self.window.resizable(False, False)
+
+        self.window.mainloop()
