30a35bdaffd9eb2c14606a125a0a14723cf98514
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2020-12-01 02:37:56 +0900

    search ip data get finish

--- gui/search_ip_page.py
+++ gui/search_ip_page.py
@@ -1,4 +1,8 @@
 from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import pymysql
+
 
 class SearchIp:
     def __init__(self):
@@ -7,12 +11,13 @@ class SearchIp:
         self.window.geometry("1000x650+250+70")
         self.window.config(bg='#272727')
         self.window.resizable(False, False)
+        self.new_s = StringVar()
 
         self.question = Label(text="학번을 입력하세요(ex.1101)", bg='#272727', fg='#51F591', font=("Arial 18 bold"))
         self.question.place(x=125, y=50)
 
-        self.colon = Label(text=":", bg='#272727', fg='#51F591', font=("Arial 18 bold"))
-        self.colon.place(x=125, y=90)
+        self.ent = Entry(bg='#272727', fg='#51F591', font=("Arial 18 bold"),textvariable=self.new_s)
+        self.ent.place(x=125, y=90)
 
         search_btn_image = PhotoImage(file='../image/search_btn.png')
         search_btn_click = Button(borderwidth=0, command=self.search, bg='#272727', activebackground='#272727')
@@ -32,4 +37,16 @@ class SearchIp:
         self.window.mainloop()
 
     def search(self):
-        pass
+        mydb = pymysql.connect(host="localhost", user="root", password="123456", db="ipc")
+        cursor = mydb.cursor()
+        hakbun = self.new_s.get()
+        query = "select hakbun,pc,phone from ip_table where hakbun=%s"
+        cursor.execute(query, (hakbun))
+        rows = cursor.fetchall()
+        data_list = list(rows[0])
+        new_ip = data_list[1]
+        new_phone = data_list[2]
+        self.update(new_ip,new_phone)
+
+    def update(self,*data):
+        print(data[1],data[2])
