d5ab767bf4ad5d2d80b0b775371c86de6d10d1b4
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-12-01 18:47:17 +0900

    student mypc page gui

--- gui/student_mypc_page.py
+++ gui/student_mypc_page.py
@@ -8,21 +8,32 @@ class MyPc:
         self.window.config(bg='#272727')
         self.window.resizable(False, False)
 
+        self.question = Label(text="학번을 입력하세요(ex.1101)", bg='#272727', fg='#51F591', font=("Arial 18 bold"))
+        self.question.place(x=80, y=50)
+
+        search_btn_image = PhotoImage(file='../image/search_btn.png')
+        search_btn_click = Button(borderwidth=0, command=self.search, bg='#272727', activebackground='#272727')
+        search_btn_click.place(x=805, y=50)
+        search_btn_click.config(image=search_btn_image)
+
         self.this_month_text = Label(text='This Month', bg='#272727', fg='#F6D875', font=("Arial 18 bold"))
-        self.this_month_text.place(x=80, y=50)
+        self.this_month_text.place(x=80, y=200)
 
         self.input_classof = Label(text='학번을 입력하세요(ex.1101)', bg='#272727', fg='#51F591', font=("Arial 18 bold"))
-        self.input_classof.place(x=80, y=200)
+        self.input_classof.place(x=80, y=350)
 
         self.input_score = Label(text='점수를 입력하세요(ex.100)', bg='#272727', fg='#51F591', font=("Arial 18 bold"))
-        self.input_score.place(x=450, y=200)
+        self.input_score.place(x=450, y=350)
 
         submit_btn_image = PhotoImage(file='../image/submit_btn.png')
         submit_btn_click = Button(borderwidth=0, command=self.submit, bg='#272727', activebackground='#272727')
-        submit_btn_click.place(x=800, y=200)
+        submit_btn_click.place(x=800, y=350)
         submit_btn_click.config(image=submit_btn_image)
 
         self.window.mainloop()
 
     def submit(self):
+        pass
+
+    def search(self):
         pass
