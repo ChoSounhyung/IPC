e67913be0a2108bd0e973fbf4416ee4952cdfe78
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-12-03 00:25:38 +0900

    first grade page chart success

--- gui/first_grade_page.py
+++ gui/first_grade_page.py
@@ -51,7 +51,6 @@ class FirstGradePage:
         dead_line = this_year+"-"+this_month+"-"+minus+" 16:30"
 
         date = datetime.datetime.strptime(dead_line, "%Y-%m-%d %H:%M")
-        print(date)
 
         count = 0
 
@@ -63,4 +62,114 @@ class FirstGradePage:
         class_1 = count
         print(class_1)
 
-        return class_1
+        query = "SELECT this_month from mypc_table where hakbun like '12%'"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        this_year = str(datetime.datetime.today().year)
+        this_month = str(datetime.datetime.today().month)
+        first_index = datetime.date(int(this_year), int(this_month), 1).weekday()  # 매달 1일의 인덱스
+        minus = str((3 - first_index) + 15)
+        dead_line = this_year + "-" + this_month + "-" + minus + " 16:30"
+
+        date = datetime.datetime.strptime(dead_line, "%Y-%m-%d %H:%M")
+
+        count = 0
+
+        for i in range(len(rows)):
+            submit_time = datetime.datetime.strptime(rows[i][0], "%Y-%m-%d %H:%M")
+            if submit_time < date:
+                count += 1
+
+        class_2 = count
+        print(class_2)
+
+        query = "SELECT this_month from mypc_table where hakbun like '13%'"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        this_year = str(datetime.datetime.today().year)
+        this_month = str(datetime.datetime.today().month)
+        first_index = datetime.date(int(this_year), int(this_month), 1).weekday()  # 매달 1일의 인덱스
+        minus = str((3 - first_index) + 15)
+        dead_line = this_year + "-" + this_month + "-" + minus + " 16:30"
+
+        date = datetime.datetime.strptime(dead_line, "%Y-%m-%d %H:%M")
+
+        count = 0
+
+        for i in range(len(rows)):
+            submit_time = datetime.datetime.strptime(rows[i][0], "%Y-%m-%d %H:%M")
+            if submit_time < date:
+                count += 1
+
+        class_3 = count
+        print(class_3)
+
+        query = "SELECT this_month from mypc_table where hakbun like '14%'"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        this_year = str(datetime.datetime.today().year)
+        this_month = str(datetime.datetime.today().month)
+        first_index = datetime.date(int(this_year), int(this_month), 1).weekday()  # 매달 1일의 인덱스
+        minus = str((3 - first_index) + 15)
+        dead_line = this_year + "-" + this_month + "-" + minus + " 16:30"
+
+        date = datetime.datetime.strptime(dead_line, "%Y-%m-%d %H:%M")
+
+        count = 0
+
+        for i in range(len(rows)):
+            submit_time = datetime.datetime.strptime(rows[i][0], "%Y-%m-%d %H:%M")
+            if submit_time < date:
+                count += 1
+
+        class_4 = count
+        print(class_4)
+
+        query = "SELECT this_month from mypc_table where hakbun like '15%'"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        this_year = str(datetime.datetime.today().year)
+        this_month = str(datetime.datetime.today().month)
+        first_index = datetime.date(int(this_year), int(this_month), 1).weekday()  # 매달 1일의 인덱스
+        minus = str((3 - first_index) + 15)
+        dead_line = this_year + "-" + this_month + "-" + minus + " 16:30"
+
+        date = datetime.datetime.strptime(dead_line, "%Y-%m-%d %H:%M")
+
+        count = 0
+
+        for i in range(len(rows)):
+            submit_time = datetime.datetime.strptime(rows[i][0], "%Y-%m-%d %H:%M")
+            if submit_time < date:
+                count += 1
+
+        class_5 = count
+        print(class_5)
+
+        query = "SELECT this_month from mypc_table where hakbun like '16%'"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        this_year = str(datetime.datetime.today().year)
+        this_month = str(datetime.datetime.today().month)
+        first_index = datetime.date(int(this_year), int(this_month), 1).weekday()  # 매달 1일의 인덱스
+        minus = str((3 - first_index) + 15)
+        dead_line = this_year + "-" + this_month + "-" + minus + " 16:30"
+
+        date = datetime.datetime.strptime(dead_line, "%Y-%m-%d %H:%M")
+
+        count = 0
+
+        for i in range(len(rows)):
+            submit_time = datetime.datetime.strptime(rows[i][0], "%Y-%m-%d %H:%M")
+            if submit_time < date:
+                count += 1
+
+        class_6 = count
+        print(class_6)
+
+        return class_1, class_2, class_3, class_4, class_5, class_6
