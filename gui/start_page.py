538e33579aac607d8605f9c5219446c1e793669d
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-12-03 13:40:22 +0900

    start page label & background color change

--- gui/start_page.py
+++ gui/start_page.py
@@ -7,22 +7,22 @@ class StartPage:
         self.window = Tk()
         self.window.title("IPC")
         self.window.geometry("1000x650+250+70")
-        self.window.config(bg='#272727')
+        self.window.config(bg='#ffffff')
         self.window.resizable(False, False)
 
-        self.title = Label(text="IPC", bg='#272727', fg='#51F591', font=("Arial 60 bold"))
-        self.explain = Label(text=": 앱설명앱설명앱설명앱설명앱설명앱설명앱설명앱설명", bg='#272727', fg='#51F591',
+        self.title = Label(text="IPC", bg='#ffffff', fg='#3F90CA', font=("Arial 60 bold"))
+        self.explain = Label(text=": 앱설명앱설명앱설명앱설명앱설명앱설명앱설명앱설명", bg='#ffffff', fg='#3F90CA',
                         font=("System 13 normal"))
         self.title.place(x=140, y=180, anchor='nw')
         self.explain.place(x=140, y=270, anchor='nw')
 
         student_image = PhotoImage(file='../image/student_btn.png')
-        student_click = Button(borderwidth=0, command=self.go_student_page, bg='#272727', activebackground='#272727')
+        student_click = Button(borderwidth=0, command=self.go_student_page, bg='#ffffff', activebackground='#ffffff')
         student_click.place(x=140, y=360)
         student_click.config(image=student_image)
 
         teacher_image = PhotoImage(file='../image/teacher_btn.png')
-        teacher_click = Button(borderwidth=0, command=self.go_teacher_page, bg='#272727', activebackground='#272727')
+        teacher_click = Button(borderwidth=0, command=self.go_teacher_page, bg='#ffffff', activebackground='#ffffff')
         teacher_click.place(x=320, y=360)
         teacher_click.config(image=teacher_image)
 
