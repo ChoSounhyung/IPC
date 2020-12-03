99d8b6d12c7a06736deb8188dfd16d49021cc321
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-12-04 00:12:23 +0900

    grade page text label add

--- gui/second_grade_page.py
+++ gui/second_grade_page.py
@@ -1,27 +1,12 @@
 from tkinter import *
-import pymysql
-import datetime
+from model.cal_time import CalTime
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
 import matplotlib.pyplot as plt
-import matplotlib
 
 from DB.db_connect import DbConnect
 
 db_connect = DbConnect()
-
-matplotlib.rcParams['font.family']='Malgun Gothic'
-matplotlib.rcParams['axes.unicode_minus'] = False
-
-this_year = datetime.datetime.today().year
-this_month = datetime.datetime.today().month
-first_index = datetime.date(this_year, this_month, 1).weekday()  # 매달 1일의 인덱스
-minus = (3 - first_index) + 15
-
-date = datetime.datetime(this_year, this_month, minus, 16, 30, 0)
-pre_date = datetime.datetime(this_year, this_month, minus, 0, 0, 0)
-print(pre_date)
 
 class SecondGradePage:
     def __init__(self):
@@ -36,11 +21,10 @@ class SecondGradePage:
 
         class_1, class_2, class_3, class_4, class_5, class_6 = self.db_connect()
 
-        a = [int(class_1), int(class_2), int(class_3), int(class_4), int(class_5), int(class_6)]
-
         x = [1, 2, 3, 4, 5, 6]
-        y = [a[0], a[1], a[2], a[3], a[4], a[5]]
-        plt.bar(x, y, color='#FF6347')
+        y = [int(class_1), int(class_2), int(class_3), int(class_4), int(class_5), int(class_6)]
+
+        plt.bar(x, y, color='#F8A9A9')
         fig = plt.figure(1)
 
         canvas = FigureCanvasTkAgg(fig, self.window)
@@ -54,137 +38,35 @@ class SecondGradePage:
         back_click.place(x=40, y=20, anchor='nw')
         back_click.config(image=back_image)
 
+        if ''.join(str(y)) != "000000":
+            for i in range(len(y)):
+                if y[i] == max(y) and y[i] != 0:
+                    self.search_ip_text = Label(text=str(i + 1) + "반이 기한내에 가장 빨리 제출했습니다.", bg='#ffffff', fg='#3F90CA',
+                                                font=("Arial 18 bold"))
+                    self.search_ip_text.place(x=300, y=100)
+
         self.window.mainloop()
 
     def db_connect(self):
+        cal_time = CalTime()
 
         query = "SELECT this_month from mypc_table where hakbun like '21%'"
-        db_connect.cursor.execute(query)
-        rows = db_connect.cursor.fetchall()
-
-        time_sum = 0
-
-        for i in range(len(rows)):
-            slice_year = int(rows[i][0][:4])
-            slice_month = int(rows[i][0][5:7])
-            slice_day = int(rows[i][0][8:10])
-            slice_hour = int(rows[i][0][11:13])
-            slice_minute = int(rows[i][0][14:])
-            submit_time = datetime.datetime(slice_year, slice_month, slice_day, slice_hour, slice_minute)
-
-            if pre_date <= submit_time:
-                time_sum += round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600)
-
-        avg = time_sum // 990
-        class_1 = str(avg)
+        class_1 = cal_time.calculate(query)
 
         query = "SELECT this_month from mypc_table where hakbun like '22%'"
-        db_connect.cursor.execute(query)
-        rows = db_connect.cursor.fetchall()
-
-        time_sum = 0
-
-        for i in range(len(rows)):
-            slice_year = int(rows[i][0][:4])
-            slice_month = int(rows[i][0][5:7])
-            slice_day = int(rows[i][0][8:10])
-            slice_hour = int(rows[i][0][11:13])
-            slice_minute = int(rows[i][0][14:])
-            submit_time = datetime.datetime(slice_year, slice_month, slice_day, slice_hour, slice_minute)
-
-            if pre_date <= submit_time:
-                time_sum += round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600)
-
-        avg = time_sum // 720
-        class_2 = str(avg)
+        class_2 = cal_time.calculate(query)
 
         query = "SELECT this_month from mypc_table where hakbun like '23%'"
-        db_connect.cursor.execute(query)
-        rows = db_connect.cursor.fetchall()
-
-        time_sum = 0
-
-        for i in range(len(rows)):
-            slice_year = int(rows[i][0][:4])
-            slice_month = int(rows[i][0][5:7])
-            slice_day = int(rows[i][0][8:10])
-            slice_hour = int(rows[i][0][11:13])
-            slice_minute = int(rows[i][0][14:])
-            submit_time = datetime.datetime(slice_year, slice_month, slice_day, slice_hour, slice_minute)
-
-            if pre_date <= submit_time:
-                time_sum += round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600)
-
-        avg = time_sum // 720
-        class_3 = str(avg)
+        class_3 = cal_time.calculate(query)
 
         query = "SELECT this_month from mypc_table where hakbun like '24%'"
-        db_connect.cursor.execute(query)
-        rows = db_connect.cursor.fetchall()
-
-        time_sum = 0
-
-        for i in range(len(rows)):
-            slice_year = int(rows[i][0][:4])
-            slice_month = int(rows[i][0][5:7])
-            slice_day = int(rows[i][0][8:10])
-            slice_hour = int(rows[i][0][11:13])
-            slice_minute = int(rows[i][0][14:])
-            submit_time = datetime.datetime(slice_year, slice_month, slice_day, slice_hour, slice_minute)
-
-            if pre_date <= submit_time:
-                time_sum += round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600)
-
-        avg = time_sum // 720
-        class_4 = str(avg)
+        class_4 = cal_time.calculate(query)
 
         query = "SELECT this_month from mypc_table where hakbun like '25%'"
-        db_connect.cursor.execute(query)
-        rows = db_connect.cursor.fetchall()
-
-        time_sum = 0
-
-        for i in range(len(rows)):
-            slice_year = int(rows[i][0][:4])
-            slice_month = int(rows[i][0][5:7])
-            slice_day = int(rows[i][0][8:10])
-            slice_hour = int(rows[i][0][11:13])
-            slice_minute = int(rows[i][0][14:])
-            submit_time = datetime.datetime(slice_year, slice_month, slice_day, slice_hour, slice_minute)
-
-            if pre_date <= submit_time:
-                time_sum += round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600)
-
-        avg = time_sum // 720
-        class_5 = str(avg)
+        class_5 = cal_time.calculate(query)
 
         query = "SELECT this_month from mypc_table where hakbun like '26%'"
-        db_connect.cursor.execute(query)
-        rows = db_connect.cursor.fetchall()
-
-        time_sum = 0
-
-        for i in range(len(rows)):
-            slice_year = int(rows[i][0][:4])
-            slice_month = int(rows[i][0][5:7])
-            slice_day = int(rows[i][0][8:10])
-            slice_hour = int(rows[i][0][11:13])
-            slice_minute = int(rows[i][0][14:])
-            submit_time = datetime.datetime(slice_year, slice_month, slice_day, slice_hour, slice_minute)
-
-            if pre_date <= submit_time:
-                time_sum += round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600)
-                print(round((date - submit_time).microseconds / float(1000000)) + (
-                        (date - submit_time).seconds + (date - submit_time).days * 24 * 3600))
-
-        avg = time_sum // 720
-        class_6 = str(avg)
+        class_6 = cal_time.calculate(query)
 
         return class_1, class_2, class_3, class_4, class_5, class_6
 
